00e080db442d5a80c7b4c0141bd40b0d8cc19034
s1ga <s.v.harlanov@gmail.com>
2023-09-14 14:29:26 +0200

    feat: init angular application

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  `,
+})
+export class AppComponent {
+}
